8390ae0a9ae75b3db455a562894a781e12da4c31
figueroar <raul.a.figueroa@hotmail.com>
2020-03-07 12:01:23 -0500

    frontend: signup form / component

--- insta-frontend/src/components/root/SignUp.js
+++ insta-frontend/src/components/root/SignUp.js
@@ -1,21 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function SignUp() {
-  return(
-    <div>
-      <form>
-          <label>User Name</label>
-          <input type="text"/>
-          <label>Email</label>
-          <input type="text"/>
-          <label>Number</label>
-          <input type="number"/>
-          <label>password</label>
-          <input type="password" />
-          <input type="submit" />
-        </form>
-    </div>
-  )
+class SignUp extends Component {
+  state = {
+    username: "",
+    email: "",  
+    phoneNum: null,
+    password: ""
+  }
+
+  handleOnChange = (event) => {
+    this.setState({
+      [event.target.name]: event.tagret.value
+    })
+  }
+
+  handleOnSubmit = (event) => {
+    event.preventDefault()
+    // this.props.addUser(this.State)
+    this.setState({
+      username: "",
+      email: "",  
+      phoneNum: null,
+      password: ""
+    })
+  }
+
+  render(){
+    return(
+      <div>
+        <form onSubmit={this.handleOnSubmit}>
+            <label>User Name</label>
+            <input type="text" name="" onChange={this.handleOnChange}/>
+            <label>Email</label>
+            <input type="text" name="" onChange={this.handleOnChange}/>
+            <label>Number</label>
+            <input type="number"name="" onChange={this.handleOnChange}/>
+            <label>password</label>
+            <input type="password" name="" onChange={this.handleOnChange}/>
+            <input type="submit" />
+          </form>
+      </div>
+    )
+  }
 }
 
 export default SignUp
