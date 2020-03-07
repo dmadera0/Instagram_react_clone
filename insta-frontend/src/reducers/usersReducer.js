bd68c19b99857d9fe210e7790260a69a2c40f401
figueroar <raul.a.figueroa@hotmail.com>
2020-03-07 14:39:40 -0500

    frontend: usersReducer structured / needs work

--- insta-frontend/src/reducers/usersReducer.js
+++ insta-frontend/src/reducers/usersReducer.js
@@ -1,5 +1,18 @@
 export default (state = { users:[], loading: false}, action ) => {
   switch(action.type){
+
+    case "ADD_USERS":
+      return state
+
+    case "CREATE_USER":
+      return state 
+    
+    case "EDIT_USER":
+      return state 
+
+    case "DELETE_USER":
+      return state
+
     default: 
     return state
   }
