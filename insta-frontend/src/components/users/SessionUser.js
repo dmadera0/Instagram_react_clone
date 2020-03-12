a08419f19f0f1419a81b4de5f16fb1526afcdcfe
figueroar <raul.a.figueroa@hotmail.com>
2020-03-12 00:10:46 -0400

    image displaying correctly in session, other elements need to display

--- insta-frontend/src/components/users/SessionUser.js
+++ insta-frontend/src/components/users/SessionUser.js
@@ -10,14 +10,13 @@ class SessionUser extends Component {
   render(){
     console.log(this.props.users)
     const thisUser = (this.props.users)
-    debugger
-    // const sessionUser = this.props.users.map( (user, i) => <User key={i} id={user.id} username={user.username} email={user.email} phoneNum={user.phoneNum} image={user.image}/>)
-    // const sessionUser = thisUser.map( (user) => <ul id={user.id} email={user.email} phoneNum={user.phoneNum}>{user.username} {user.image}</ul>)
+    const image = <img src={thisUser.image} width="200" alt="" />;
+    const username = <p>{thisUser.username}</p>   
     debugger
     return(
       <div>
         <p>Session user</p>
-        {this.props.laoding ? <h3>...loading...</h3> : (thisUser.username ) }      
+        {this.props.laoding ? <h3>...loading...</h3> : username, image}      
       </div>
     )
   }
