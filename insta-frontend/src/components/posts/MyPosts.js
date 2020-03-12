f353ae7a0e1b037cc1bde5288e8b32c300bcc14b
figueroar <raul.a.figueroa@hotmail.com>
2020-03-12 19:09:49 -0400

    frontend: cleaner usersHome display

--- insta-frontend/src/components/posts/MyPosts.js
+++ insta-frontend/src/components/posts/MyPosts.js
@@ -2,13 +2,11 @@ import React, {Component} from 'react'
 
 class MyPosts extends Component {
   render(){
-  const Myposts = this.props.posts.map( (post, i) => <li key={i} id={post.id} user_id={post.user_id}><img src={post.image} width="400" alt=""/></li>)
+  const Myposts = this.props.posts.map( (post, i) => <ul key={i} id={post.id} user_id={post.user_id}><img src={post.image} width="400" alt=""/></ul>)
   
     return(
       <div>
       {Myposts}
-      <p>My Posts</p>
-      
     </div>)
   }
 }
