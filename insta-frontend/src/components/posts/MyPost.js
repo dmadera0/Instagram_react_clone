a00767907da627d403116ee62910c4fe57ea7daa
figueroar <raul.a.figueroa@hotmail.com>
2020-03-15 00:16:26 -0400

    comment button now redirects / needs props in roting

--- insta-frontend/src/components/posts/MyPost.js
+++ insta-frontend/src/components/posts/MyPost.js
@@ -1,15 +1,15 @@
-import React, { useState, useHistory} from 'react'
-
+import React, { useState } from 'react'
+import { useHistory, Link} from "react-router-dom";
 function MyPost(props) {
   const [ like, setLike] = useState('♡')
-
+  let history = useHistory()
   
   return(
     <div>
       {<br></br>}
       {props.content}{<br></br>}
-      {<img src={props.image } width="400" alt=""/>}{<br></br>}
-      <button onClick={ e => setLike('❤️')}>{like} </button><button>💬</button>
+      {<img src={props.image } width="400" alt="" />}{<br></br>}
+      <button onClick={ e => setLike('❤️')}>{like} </button> <button onClick={ e => history.push('/comment')}>💬</button>
       {<br></br>}
       {<br></br>}
     </div>)
