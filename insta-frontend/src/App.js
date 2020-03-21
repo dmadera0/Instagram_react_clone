d8214cf1e52e8387f1a6158b89ed25111740edea
figueroar <raul.a.figueroa@hotmail.com>
2020-03-21 17:57:25 -0400

    import 'bootstrap/dist/css/bootstrap.min.css';

--- insta-frontend/src/App.js
+++ insta-frontend/src/App.js
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import React, { Component }  from 'react';
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import { connect } from 'react-redux'
